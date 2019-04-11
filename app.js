6d428c24c6297e5abca28a084a2d4deebcf05d64
raj0109 <er.ranjeetjain@gmail.com>
2019-04-12 02:03:13 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,49 @@
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
+const fs = require('fs');
+
+
+// create directory if does not exist
+const dir = './logs';
+if (!fs.existsSync(dir)){
+    fs.mkdirSync(dir);
+}
+
+fs.appendFileSync('./logs/1.txt', "")
+
+var app = express();
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.post('/bill', function(req, res) {
+  let data = req.body;
+  if (data instanceof Object && data.BillNumber) {
+    const data =  JSON.stringify(req.body);
+    fs.appendFileSync('./logs/1.txt', data + ',')
+    res.sendStatus(200)      
+  } else {
+    res.sendStatus(400)
+  }
+});
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
